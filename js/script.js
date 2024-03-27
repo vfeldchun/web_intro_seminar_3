ba807f1e7059f8a28add614b8042d2b3547223c3
vfeldchun <vladuz@mail.ru>
2024-03-27 23:08:40 +0300

    Homework to Seminar 3 added!

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", ShowName);
+
+function ShowName() {
+  let isGotName = false;
+  var userName;
+
+  while (!isGotName) {
+    userName = prompt("Введите имя пользователя");
+    // Если имя пользователя не соответсвует шаблону
+    if (ValidateName(userName)) {
+      isGotName = true;
+    }
+  }
+  // Если отмена то ничего не выводим
+  if (userName != null) {
+    PrintName(userName);
+  }
+}
+
+function ValidateName(name) {
+  // Проверка на шаблон
+  // Имя пользователя может содержать буквы в латинице или кирилице, цифры и символ '_'
+  return /^[a-zA-Zа-яА-Я0-9_]+$/.exec(name);
+}
+
+function PrintName(userName) {
+  alert("Имя пользователя: " + userName);
+}
